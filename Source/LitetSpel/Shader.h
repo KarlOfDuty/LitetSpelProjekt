fe61251eb4fc25765603fb0ea02b7c5e69a0c4fa
Zather1995 <zather1995@gmail.com>
2017-04-03 15:49:54 +0200

    added hello triangle, glm. shader and camera class

---
+++ Source/LitetSpel/Shader.h
@@ -0,0 +1,22 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+	GLuint program;
+	// Constructor generates the shader on the fly
+	Shader();
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	// Uses the current shader
+	void use();
+};
+
+#endif
