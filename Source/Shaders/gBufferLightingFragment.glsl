dce5716516440081f7dc0951abf5519146b07f81
Walter Karlqvist <walter.karlqvist@gmail.com>
2017-05-02 10:43:44 +0200

    soon working dual shadows

--- Source/Shaders/gBufferLightingFragment.glsl
+++ Source/Shaders/gBufferLightingFragment.glsl
@@ -23,7 +23,7 @@ uniform vec3 viewPos;
 uniform mat4 lightSpaceMatrix;
 uniform mat4 lightSpaceMatrix2;
 
-float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal, vec3 lightDirection)
+float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal, vec3 lightDirection, sampler2D depthM)
 {
 	// Perform perspective divide
 	vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
@@ -35,14 +35,14 @@ float ShadowCalculation(vec4 fragPosLightSpace, vec3 normal, vec3 lightDirection
 	// Calculate bias
 	float bias = max(0.05 * (1.0 - dot(normal, lightDirection)), 0.005);   
 	// PCF - Percentage-Closer Filtering - used to offset the texture coordinates
-	vec2 texelSize = 1.0 / textureSize(depthMap, 0);
+	vec2 texelSize = 1.0 / textureSize(depthM, 0);
 
 	// Check wheter current frag pos is in shadow
 	for(int x = -1; x <= 1; x++)
 	{
 		for(int y = -1; y <= 1; y++)
 		{
-			float pcfDepth = texture(depthMap, projCoords.xy + vec2(x, y) * texelSize).r;
+			float pcfDepth = texture(depthM, projCoords.xy + vec2(x, y) * texelSize).r;
 			if(currentDepth - bias > pcfDepth)
 			{
 				shadow += 1.0;
@@ -66,17 +66,10 @@ void main()
 	//Adds the ambient
 	vec3 lighting = ambient;
 	vec3 viewDir = normalize(viewPos - fragPos);
-	vec4 lightSpaces[10];
+	vec4 lightSpaces[3];
 	lightSpaces[0] = lightSpaceMatrix * vec4(fragPos, 1.0);
 	lightSpaces[1] = lightSpaceMatrix2 * vec4(fragPos, 1.0);
 	lightSpaces[2] = lightSpaceMatrix * vec4(fragPos, 1.0);
-	lightSpaces[3] = lightSpaceMatrix * vec4(fragPos, 1.0);
-	lightSpaces[4] = lightSpaceMatrix * vec4(fragPos, 1.0);
-	lightSpaces[5] = lightSpaceMatrix * vec4(fragPos, 1.0);
-	lightSpaces[6] = lightSpaceMatrix * vec4(fragPos, 1.0);
-	lightSpaces[7] = lightSpaceMatrix * vec4(fragPos, 1.0);
-	lightSpaces[8] = lightSpaceMatrix * vec4(fragPos, 1.0);
-	lightSpaces[9] = lightSpaceMatrix * vec4(fragPos, 1.0);
 	vec4 fragPosLightSpace = lightSpaceMatrix * vec4(fragPos, 1.0);
 	vec4 fragPosLightSpace2 = lightSpaceMatrix2 * vec4(fragPos, 1.0);
 	 
@@ -93,7 +86,15 @@ void main()
 		float spec = pow(max(dot(normal, halfwayDir), 0.0), 16.0);
 		vec3 thisSpecular = lights[i].color * spec * specular;
 		// Calculate shadows
-		float shadow = ShadowCalculation(lightSpaces[i], normal, lightDir);
+		float shadow;
+		if(i != 1)
+		{
+			shadow = ShadowCalculation(lightSpaces[i], normal, lightDir, depthMap);
+		}
+		else
+		{
+			shadow = ShadowCalculation(lightSpaces[i], normal, lightDir, depthMap2);
+		}
         thisDiffuse *= attenuation;
         thisSpecular *= attenuation;
 		lighting += (1.0 - shadow) * (thisDiffuse + thisSpecular);
