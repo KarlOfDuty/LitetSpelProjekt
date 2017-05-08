ae6423cbcb32ebfaf52017420f76fbafb1fbe72b
Zather1995 <zather1995@gmail.com>
2017-05-08 15:45:42 +0200

    melee and structured up class okayer

--- Source/PlayerBird.cpp
+++ Source/PlayerBird.cpp
@@ -4,6 +4,7 @@ PlayerBird::PlayerBird(int health, Model model, bool inWater) :PlayerChar(health
 {
 	this->maxJumps = 2;
 	this->jumpHeight = 15;
+	box = new Model("models/cube/cubeGreen.obj");
 }
 
 PlayerBird::~PlayerBird()
@@ -28,4 +29,13 @@ float PlayerBird::getJumpHeight()
 void PlayerBird::waterEffect()
 {
 	this->setHealth(0);
+}
+
+void PlayerBird::meleeAttack(std::vector<Projectile*> &allAttackBoxes, glm::vec2 position, glm::vec2 direction, float velocity)
+{
+	glm::vec3 scale(0, 3.0f, 1.0f);
+	position.y += 1.5f;
+	Projectile* temp = new Projectile;
+	temp->melee(box, position, direction, velocity, scale);
+	allAttackBoxes.push_back(temp);
 }
