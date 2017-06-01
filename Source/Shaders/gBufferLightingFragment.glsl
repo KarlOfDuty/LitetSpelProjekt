48b9ca4e583a7bf4bad4c27e6bdaeca19b843160
Karl Essinger <xkaess22@gmail.com>
2017-06-01 20:40:00 +0200

    Ambient increase

--- Source/Shaders/gBufferLightingFragment.glsl
+++ Source/Shaders/gBufferLightingFragment.glsl
@@ -75,7 +75,7 @@ void main()
 	float specular = texture(gAlbedoSpec, texCoords).a;
 	vec3 ambient = texture(gAmbient, texCoords).rgb;
 	//Ambient is not used because of reasons
-	vec3 lighting = diffuse*0.2f;
+	vec3 lighting = diffuse*0.4f;
 	vec3 viewDir = normalize(viewPos - fragPos);
 
 	for(int i = 0; i < numberOfDirLights; i++)
